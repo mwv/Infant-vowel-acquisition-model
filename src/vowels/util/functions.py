f13a3ccf2fec3c98392f1dd9f0a56ff0ef4c8130
Maarten Versteegh <maartenversteegh@gmail.com>
2012-04-16 17:58:43 +0200

    moved _float to util.functions

--- src/vowels/util/functions.py
+++ src/vowels/util/functions.py
@@ -33,3 +33,11 @@ def mel_to_hertz(m):
 
 def hertz_to_bark(f):
     return 13 * np.arctan(0.00076 * f) + 3.5 * np.arctan((f/7500)**2)
+
+def _float(s):
+    """wrapper for float, but returns np.nan if s cannot be converted
+    """
+    try:
+        return float(s)
+    except ValueError:
+        return np.nan
