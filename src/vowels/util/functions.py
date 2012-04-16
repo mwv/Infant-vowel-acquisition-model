41322524986a215e9bf9a910168fff2e61fc38d7
Maarten Versteegh <maartenversteegh@gmail.com>
2012-04-16 17:18:48 +0200

    added utility functions for frequency conversion

---
+++ src/vowels/util/functions.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------
+# author:
+# Maarten Versteegh
+# http://lands.let.ru.nl/~versteegh/
+# maartenversteegh AT gmail DOT com
+# Centre for Language Studies
+# Radboud University Nijmegen
+# 
+# Licensed under GPLv3
+# ------------------------------------
+
+from __future__ import division
+
+import numpy as np
+
+'''
+vowels.util.functions:
+
+utility functions
+
+'''
+
+# frequency conversion
+
+def hertz_to_mel(f):
+    return 2595 * np.log10(1+f/700)
+
+def mel_to_hertz(m):
+    return 700 * (10**(m/2595)-1)
+
+def hertz_to_bark(f):
+    return 13 * np.arctan(0.00076 * f) + 3.5 * np.arctan((f/7500)**2)
