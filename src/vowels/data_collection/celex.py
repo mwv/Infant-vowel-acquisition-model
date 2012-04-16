bd418a5aa4447da1c1bcf0fadb2f4deb300b3046
Maarten Versteegh <maartenversteegh@gmail.com>
2012-04-16 17:46:36 +0200

    moved disc to sampa conversion map to util.transcript_formats

--- src/vowels/data_collection/celex.py
+++ src/vowels/data_collection/celex.py
@@ -29,6 +29,7 @@ import os
 
 from bidict import bidict
 from ..config.paths import cfg_celexdir
+from ..util.transcipt_formats import disc_to_sampa
 
 CELEX_FILE = os.path.join(cfg_celexdir, 'DUTCH/DPW/DPW.CD')
 
@@ -63,95 +64,10 @@ def stripStressMarkers(trans):
     return re.sub("\'","", trans)
 
 def transToSampa(trans):
-    return map(lambda x:disc_to_sampa[x], list(trans))
+    return map(lambda x:disc_to_sampa(x), list(trans))
 
 def stripSyllableMarkers(trans):
     return re.sub("-","",trans)
 
 
 
-disc_to_sampa = bidict({
-    ' ':' ',
-    'p':'p',
-    'b':'b',
-    't':'t',
-    'd':'d',
-    'k':'k',
-    'g':'g',
-    'N':'N',
-    'm':'m',
-    'n':'n',
-    'l':'l',
-    'r':'r',
-    'f':'f',
-    'v':'v',
-    'T':'T',
-    'D':'D',
-    's':'s',
-    'z':'z',
-    'S':'S',
-    'Z':'Z',
-    'j':'j',
-    'x':'x',
-    'G':'G',
-    'h':'h',
-    'w':'w',
-    '+':'pf',
-    '=':'ts',
-    'J':'tS',
-    '_':'dZ',
-    'C':'N,',
-    'F':'m,',
-    'H':'n,',
-    'P':'l,',
-    'R':'r*',
-    'i':'i:',
-    '!':'i::',
-    '#':'A:',
-    'a':'a:',
-    '$':'0:',
-    'u':'u:',
-    '3':'3:',
-    'y':'y:',
-    '(':'y::',
-    ')':'E:',
-    '*':'/:',
-    '<':'Q:',
-    'e':'e:',
-    '|':'|:',
-    'o':'o:',
-    '1':'eI',
-    '2':'aI',
-    '4':'OI',
-    '5':'@U',
-    '6':'aU',
-    '7':'I@',
-    '8':'E@',
-    '9':'U@',
-    'K':'EI',
-    'L':'/I',
-    'M':'Au',
-    'W':'ai',
-    'B':'au',
-    'X':'Oy',
-    'I':'I',
-    'Y':'Y',
-    'E':'E',
-    '/':'/',
-    '{':'{',
-    '&':'{',
-    'A':'A',
-    'Q':'Q',
-    'V':'V',
-    'O':'O',
-    'U':'U',
-    '}':'}',
-    '@':'@',
-    '^':'/~:',
-    'c':'{~',
-    'q':'A~:',
-    '0':'{~:',
-    '-':'-',
-    "'":"\"",
-    "\"":'%',
-    '.':'.'})
