132a544a19c12e6d54eb5ada2ef6134ea4ef7283
Maarten Versteegh <maartenversteegh@gmail.com>
2012-05-01 04:14:02 +0200

    added dimensionality reduction functions, will remove/update in future

--- src/vowels/clustering/manifold.py
+++ src/vowels/clustering/manifold.py
@@ -18,14 +18,24 @@ vowels.clustering.manifold:
 compare manifold learning methods
 '''
 
+import numpy as np
+
 from time import time
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 
 from sklearn.utils.fixes import qr_economic
-from sklearn import manifold, decomposition, lda
+from sklearn import manifold, decomposition, lda, qda
 
+def pca(X, y, n_components=None):
+    X_pca = decomposition.RandomizedPCA(n_components=n_components).fit_transform(X, y)
+    return X_pca
 
+def lda(X, y, n_components=None):
+    X2 = X.copy()
+    X2.flat[::X.shape[1] + 1] += 0.01
+    X2.flat = lda.LDA(n_components=n_components).fit_transform(X2, y)
+    
+def isomap(X, y, n_neighbors=None):
+    X_iso = manifold.Isomap(n_neighbors=n_neighbors,method='standard').fit_transform(X, y)
+    return X_iso
 
 
